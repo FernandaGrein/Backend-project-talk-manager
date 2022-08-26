134168945de809b1978e77ad634196fc0e834f9f
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-08-26 18:23:54 -0300

    corrige erro no lint

--- src/middlewares/emailValidation.js
+++ src/middlewares/emailValidation.js
@@ -1,6 +1,6 @@
 const emailValidation = (req, res, next) => {
   const { email } = req.body;
-  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
